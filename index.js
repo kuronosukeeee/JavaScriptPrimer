e7a3adeeaa54c18e5871af029f44003f5a38413f
kuronosukeeee <kuronosukeeee@gmail.com>
2023-03-08 09:52:34 +0900

    データ型とリテラル

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log(1);
+console.log(1 + 1);
+
+const total = 42 + 42;
+console.log(total);
+console.log("JavaScript");
+
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof 9000000000000n);
+console.log(typeof "JavaScript");
+console.log(typeof Symbol("シンボル"));
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof ["配列"]);
+console.log(typeof {"key":"value"});
+console.log(typeof function(){});
+
+console.log(Number.MAX_SAFE_INTEGER)
+
